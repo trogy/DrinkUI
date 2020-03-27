1860003572825bd5a61393ca4081d13cd945fee8
trogy <33707105+trogy@users.noreply.github.com>
2020-03-27 22:40:36 +1300

    Added new UI screens

---
+++ UIConcepts/UI1/JS/Loaders/home.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#btn-right").click(function(){
+        $("#AJAXContent").load("Pages/Dispense.php");
+    });
+});
+$(document).ready(function(){
+    $("#btn-left").click(function(){
+        $("#AJAXContent").load("Pages/Configure.php");
+    });
+});
